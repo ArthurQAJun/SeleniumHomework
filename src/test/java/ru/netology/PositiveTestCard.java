5c5c253649a1d4e09fdc3e9fd14a6d4d7563a13a
Arthur <rutra.ru@yandex.ru>
2023-07-02 21:04:15 +0500

    Add Test

--- src/test/java/ru/netology/PositiveTestCard.java
+++ src/test/java/ru/netology/PositiveTestCard.java
@@ -5,15 +5,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PositiveTestCard {
     private WebDriver driver;
 
     @BeforeAll
     public static void SetupAll() {
+
         WebDriverManager.chromedriver().setup();
     }
 
@@ -22,6 +27,7 @@ public class PositiveTestCard {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 
@@ -31,5 +37,16 @@ public class PositiveTestCard {
         driver = null;
     }
 
+    @Test
+    public void ShouldTestPositive() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Зубенко Михаил Петрович");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79991442566");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected ="  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText();
+        assertEquals(expected,actual);
+    }
 
 }
